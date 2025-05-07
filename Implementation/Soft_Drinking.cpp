83a8ebd3ee03f2df7149e4c731a8bdc73c513b36
Ananya150204 <ananyathegreat4@gmail.com>
2025-05-08 02:18:16 +0530

    Organised things

--- Implementation/Soft_Drinking.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main() {
-    int n, k, l, c, d, p, nl, np;
-    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
-    int mini = min((k*l)/nl, min(c*d,p/np));
-    cout << mini/n << endl;
-    return 0;
-}

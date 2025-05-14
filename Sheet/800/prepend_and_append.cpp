547253207df01e846e22537ef65baeac15553682
Ananya150204 <ananyathegreat4@gmail.com>
2025-05-15 02:26:55 +0530

    added some codes

--- Sheet/800/prepend_and_append.cpp
+++ Sheet/800/prepend_and_append.cpp
@@ -4,7 +4,24 @@ using namespace std;
 #define ll long long
 
 void ak(){
-    
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if (s[0] == s[n-1]){
+        cout << n << endl;
+        return;
+    }
+    int i = 0;
+    int j = n-1;
+    while (i < j){
+        if (s[i] == s[j]){
+            break;
+        }
+        i++;
+        j--;
+    }
+    cout << j-i+1 << endl;
 }
 int32_t main(){
     ios_base::sync_with_stdio(false); cin.tie(0);
